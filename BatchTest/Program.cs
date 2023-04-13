521167d7e8eabbbdc29efc89a6975d07cd12236f
ShinnnosukeNakamura <ShinnnosukeNakamura@users.noreply.github.com>
2023-04-13 22:22:45 +0900

    Swagger有効化？

--- BatchTest/Program.cs
+++ BatchTest/Program.cs
@@ -11,9 +11,20 @@ builder.Services.AddControllers();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Your API Title", Version = "v1" });
+});
 
 var app = builder.Build();
+
+bool enableSwagger = builder.Configuration.GetValue<bool>("Swagger:Enabled");
+// Swagger UI‚ÌÝ’è‚ð’Ç‰Á
+if (enableSwagger)
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Title v1"));
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
