5a6834d5e5107186713f742a28a31ee7c9190975
ShinnnosukeNakamura <ShinnnosukeNakamura@users.noreply.github.com>
2023-04-23 22:16:29 +0900

    データ選択画面のモックを追加

---
+++ BatchTaskDashboard/Model/FilterParam.cs
@@ -0,0 +1,9 @@
+﻿namespace BatchTaskDashboard.Model
+{
+    public class FilterParam
+    {
+        public string Filter1 { get; set; }
+        public string Filter2 { get; set; }
+        public string Itemname { get; set; }
+    }
+}
