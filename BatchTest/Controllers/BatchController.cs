5ecbb581005d60d5fe8f481ef6ceb082abdece3d
ShinnnosukeNakamura <ShinnnosukeNakamura@users.noreply.github.com>
2023-05-17 06:19:21 +0900

    バッチ登録複製対応

--- BatchTest/Controllers/BatchController.cs
+++ BatchTest/Controllers/BatchController.cs
@@ -25,16 +25,59 @@ namespace BatchTest.Controllers
         [HttpPost]
         public async Task<IActionResult> RegisterBatchTask([FromBody] BatchTaskInfo taskInfo)
         {
-            // タスクファイル名を作成
-            string fileName = $"{taskInfo.ScheduledStartTime:yyyyMMddHHmmss}_{taskInfo.GUID}_{taskInfo.Email}.json";
-
-            // appsettings.jsonで指定したディレクトリにタスクファイルを保存
-            var taskFilePath = Path.Combine(_taskDirectory, fileName);
-            System.IO.File.WriteAllText(taskFilePath, JsonConvert.SerializeObject(taskInfo));
-
+            // バッチの種類に応じた処理
+            switch (taskInfo.BatchType)
+            {
+                case BatchType.Immediate:
+                    // 即時バッチの場合、タスクファイルをそのまま作成
+                    string immediateFileName = $"{taskInfo.ScheduledStartTime:yyyyMMddHHmmss}_{taskInfo.GUID}_{taskInfo.Email}.json";
+                    WriteTaskFile(_taskDirectory, immediateFileName, taskInfo);
+                    break;
+                case BatchType.Daily:
+                    // 日次バッチの場合、指定された回数分のタスクファイルを作成
+                    for (int i = 0; i < taskInfo.LoopCount; i++)
+                    {
+                        var dailyTaskInfo = new BatchTaskInfo
+                        {
+                            BatchName = taskInfo.BatchName,
+                            GUID = taskInfo.GUID,
+                            ScheduledStartTime = taskInfo.ScheduledStartTime.AddDays(i),
+                            Email = taskInfo.Email,
+                            PostInfos = new List<PostInfo>(taskInfo.PostInfos)
+                        };
+                        string dailyFileName = $"{dailyTaskInfo.ScheduledStartTime:yyyyMMddHHmmss}_{dailyTaskInfo.GUID}_{dailyTaskInfo.Email}.json";
+                        WriteTaskFile(_taskDirectory, dailyFileName, dailyTaskInfo);
+                    }
+                    break;
+                case BatchType.Monthly:
+                    // 月次バッチの場合、指定された回数分のタスクファイルを作成
+                    for (int i = 0; i < taskInfo.LoopCount; i++)
+                    {
+                        var monthlyTaskInfo = new BatchTaskInfo
+                        {
+                            BatchName = taskInfo.BatchName,
+                            GUID = taskInfo.GUID,
+                            ScheduledStartTime = taskInfo.ScheduledStartTime.AddMonths(i),
+                            Email = taskInfo.Email,
+                            PostInfos = new List<PostInfo>(taskInfo.PostInfos)
+                        };
+                        string monthlyFileName = $"{monthlyTaskInfo.ScheduledStartTime:yyyyMMddHHmmss}_{monthlyTaskInfo.GUID}_{monthlyTaskInfo.Email}.json";
+                        WriteTaskFile(_taskDirectory, monthlyFileName, monthlyTaskInfo);
+                    }
+                    break;
+            }
 
             return Ok();
         }
+
+        private void WriteTaskFile(string directory, string fileName, BatchTaskInfo taskInfo)
+        {
+            // appsettings.jsonで指定したディレクトリにタスクファイルを保存
+            var taskFilePath = Path.Combine(directory, fileName);
+            System.IO.File.WriteAllText(taskFilePath, JsonConvert.SerializeObject(taskInfo));
+        }
+
+
         [HttpPost("get-tasks")]
         public IActionResult GetTasks([FromBody] EmailRequest emailRequest)
         {
--- BatchTest/Model/BatchTaskInfo.cs
+++ BatchTest/Model/BatchTaskInfo.cs
@@ -1,5 +1,11 @@
 ﻿namespace BatchTest.Model
 {
+    public enum BatchType
+    {
+        Immediate,
+        Daily,
+        Monthly
+    }
     public class BatchTaskInfo
     {
         public string BatchName { get; set; }
@@ -7,5 +13,7 @@
         public DateTimeOffset ScheduledStartTime { get; set; }
         public string Email { get; set; }
         public List<PostInfo> PostInfos { get; set; }
+        public BatchType BatchType { get; set; } // バッチの種類
+        public int LoopCount { get; set; } // ループの回数
     }
 }
